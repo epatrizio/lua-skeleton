832187f26499bdf8152e6e9d116c0f1bd7730fad
Eric Patrizio <epatrizio@mpns.fr>
2026-07-28 11:27:47 +0200

    refact: user module

--- src/chat/client.lua
+++ src/chat/client.lua
@@ -1,29 +1,7 @@
-local sha1 = require("sha1")
-
+local cuser = require("chat.user")
 local messages = require("chat.messages")
 
 local client = {}
-
-local function user_exists(users, username)
-    for _, user in pairs(users) do
-        if user.username == username then
-            return true
-        end
-    end
-    return false
-end
-
-local function user_password(username, password)
-    local pwd_sha1 = sha1.sha1(password)
-    for line in io.lines(_G.Users_file) do
-        local i1, _ = string.find(line, username .. ":")
-        local i2, _ = string.find(line, ":" .. pwd_sha1)
-        if i1 ~= nil and i1 > 0 and i2 ~= nil and i2 > 0 then
-            return true
-        end
-    end
-    return false
-end
 
 function client.help(user)
     print("user help " .. user.username)
@@ -41,21 +19,20 @@ function client.login(tcp_client, users)
     end
     tcp_client:send(messages.user_login())
     local username, err = tcp_client:receive()
-    if users[tcp_client] ~= nil or user_exists(users, username) then
+    if users[tcp_client] ~= nil or cuser.is_connected_user(users, username) then
         return nil, error(messages.user_login_error(username))
     else
         if not err then
             tcp_client:send(messages.user_password())
             local pwd, _err = tcp_client:receive()
-            if user_password(username, pwd) then
+            if cuser.is_valid_login(username, pwd) then
                 print("user login " .. username)
                 tcp_client:settimeout(nil)
                 tcp_client:setoption("keepalive", true)
                 tcp_client:send(messages.user_welcome(username))
                 tcp_client:send(messages.help())
                 tcp_client:send(messages.user_prompt(username))
-                local user = require("chat.user")
-                return user.new(username, tcp_client)
+                return cuser.new(username, tcp_client)
             else
                 return nil, error(messages.user_password_error())
             end
--- src/chat/user.lua
+++ src/chat/user.lua
@@ -1,3 +1,5 @@
+local sha1 = require("sha1")
+
 local user = {}
 
 ---Create a user record { username: string, tcp_client: socket.client }
@@ -7,6 +9,27 @@ local user = {}
 ---@return table: user table record { username: string, tcp_client: socket.client }
 function user.new(username, tcp_client)
     return { username = username, tcp_client = tcp_client }
+end
+
+function user.is_valid_login(username, password)
+    local pwd_sha1 = sha1.sha1(password)
+    for line in io.lines(_G.Users_file) do
+        local i1, _ = string.find(line, username .. ":")
+        local i2, _ = string.find(line, ":" .. pwd_sha1)
+        if i1 ~= nil and i1 > 0 and i2 ~= nil and i2 > 0 then
+            return true
+        end
+    end
+    return false
+end
+
+function user.is_connected_user(users, username)
+    for _, cuser in pairs(users) do
+        if cuser.username == username then
+            return true
+        end
+    end
+    return false
 end
 
 return user
